a644148198a96b83b5ec99ae6da535b780d2100d
Alexandre Junior <alexandrejunior408@gmail.com>
2022-06-10 17:11:22 -0300

    cria método para enviar no email quando perder a senha e também foi montada a página de perdeu a senha com o reaproveitamento de componentes já criados

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -108,3 +108,16 @@ export const PHOTO_DELETE = (id) => {
     },
   };
 };
+
+export const PASSWORD_LOST = (body) => {
+  return {
+    url: API_URL + '/api/password/lost',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+};
--- src/components/login/LoginPasswordLost.jsx
+++ src/components/login/LoginPasswordLost.jsx
@@ -1,3 +1,45 @@
+import { Input } from '../form/Input';
+import { Button } from '../form/Button';
+import { useForm } from '../../hooks/useForm';
+import { useFetch } from '../../hooks/useFetch';
+import { PASSWORD_LOST } from '../../api/Api';
+import { Error } from '../helper/Error';
+
 export const LoginPasswordLost = () => {
-  return <div>LoginPasswordLost</div>;
+  const login = useForm();
+
+  const { data, loading, error, request } = useFetch();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (login.validate()) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace('perdeu', 'resetar'),
+      });
+
+      const { json } = await request(url, options);
+      console.log(json);
+    }
+  };
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      {data ? (
+        <p style={{ color: '#4c1' }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="login" {...login} />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar Email</Button>
+          )}
+        </form>
+      )}
+
+      <Error error={error} />
+    </section>
+  );
 };
